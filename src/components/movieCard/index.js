ada7b776ad772133b4245017e4c48598720b9f4b
AdelleMcateer <adellemaca@gmail.com>
2021-07-11 14:52:58 +0100

    Bug Fix - make action icons on movie card configurable

--- src/components/movieCard/index.js
+++ src/components/movieCard/index.js
@@ -1,5 +1,7 @@
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
@@ -12,10 +14,9 @@ import CalendarIcon from "@material-ui/icons/CalendarTodayTwoTone";
 import StarRateIcon from "@material-ui/icons/StarRate";
 import IconButton from "@material-ui/core/IconButton";
 import Grid from "@material-ui/core/Grid";
-import img from '../../images/film-poster-placeholder.png'
-import { Link } from "react-router-dom";
-import Avatar from "@material-ui/core/Avatar";
-import { MoviesContext } from "../../contexts/moviesContext";
+import img from '../../images/film-poster-placeholder.png';
+import { MoviesContext } from "../../contexts/moviesContext"
+
 
 const useStyles = makeStyles({
     card: { maxWidth: 345 },
@@ -25,7 +26,7 @@ const useStyles = makeStyles({
     },
 });
 
-export default function MovieCard({ movie }) {
+export default function MovieCard({ movie, action }) {
     const classes = useStyles();
     const { favorites, addToFavorites } = useContext(MoviesContext);
 
@@ -82,9 +83,7 @@ export default function MovieCard({ movie }) {
                 </Grid>
             </CardContent>
             <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites" onClick={handleAddToFavorite}>
-                    <FavoriteIcon color="primary" fontSize="large" />
-                </IconButton>
+                {action(movie)}
                 <Link to={`/movies/${movie.id}`}>
                     <Button variant="outlined" size="medium" color="primary">
                         More Info ...
