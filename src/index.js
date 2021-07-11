7eae93f021551a805d8078f6dc01d3f533b9bd3d
AdelleMcateer <adellemaca@gmail.com>
2021-07-11 13:16:18 +0100

    Full review page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
+import MovieReviewPage from "./pages/movieReviewPage";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
-import FavoriteMoviesPage from "./pages/favoriteMoviesPage"; // NEW
+import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
 
 const App = () => {
     return (
@@ -17,6 +18,7 @@ const App = () => {
                 </li>
             </ul>
             <Switch>
+                <Route path="/reviews/:id" component={MovieReviewPage} />
                 <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
                 <Route path="/movies/:id" component={MoviePage} />
                 <Route exact path="/" component={HomePage} />
