07fd8db026658b2528e2614c88c56bfcf035e501
AdelleMcateer <adellemaca@gmail.com>
2021-07-11 10:20:45 +0100

    Setup Font Awesome library

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import HomePage from "./pages/homePage";
-import MoviePage from "./pages/movieDetailsPage";
+import MoviePage from './pages/movieDetailsPage'
 
 const App = () => {
     return (
         <BrowserRouter>
-            <Switch>
-                <Route path="/movies/:id" component={MoviePage} />
-                <Route path="/" component={HomePage} />
-                <Redirect from="*" to="/" />
-            </Switch>
+            <div className="jumbotron">
+                <div className="container-fluid">
+                    <Switch>
+                        <Route path="/movies/:id" component={MoviePage} />
+                        <Route path="/" component={HomePage} />
+                        <Redirect from="*" to="/" />
+                    </Switch>
+                </div>
+            </div>
         </BrowserRouter>
     );
 };
