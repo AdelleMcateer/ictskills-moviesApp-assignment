5c32362a2fc9e61f2199dbc3c07c3c24101101f4
AdelleMcateer <adellemaca@gmail.com>
2021-07-11 11:50:20 +0100

    Add to favourites logic completed; Dummy favourites page.

--- src/pages/favoriteMoviesPage.js
+++ src/pages/favoriteMoviesPage.js
@@ -1,18 +1,7 @@
 import React from "react";
-import PageTemplate from "../components/templateMovieListPage";
 
-const FavoriteMoviesPage = (props) => {
-    const toDo = () => true;
-    // Get movies from local storage.
-    const movies = JSON.parse(localStorage.getItem("favorites"));
+const FavoriteMoviesPage = () => {
+    return <h2>Favorite Movies</h2>
+}
 
-    return (
-        <PageTemplate
-            title="Favourite Movies"
-            movies={movies}
-            selectFavorite={toDo}
-        />
-    );
-};
-
-export default FavoriteMoviesPage;
+export default FavoriteMoviesPage
