610bb353d5300b26d9359031a65fee73a3e4de6a
AdelleMcateer <adellemaca@gmail.com>
2021-07-11 15:14:45 +0100

    Add new review support

--- src/components/cardIcons/writeReview.js
+++ src/components/cardIcons/writeReview.js
@@ -1,9 +1,19 @@
 import React from "react";
 import RateReviewIcon from "@material-ui/icons/RateReview";
+import { Link } from "react-router-dom";
 
 const WriteReviewIcon = ({ movie }) => {
     return (
-        <RateReviewIcon color="primary" fontSize="large" />
+        <Link
+            to={{
+                pathname: `/reviews/form`,
+                state: {
+                    movieId: movie.id,
+                },
+            }}
+        >
+            <RateReviewIcon color="primary" fontSize="large" />
+        </Link>
     );
 };
 
